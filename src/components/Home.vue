d63d4b10bb7b6750cfaee648e84a188cdcf698d4
Thom David <thomdavid74@gmail.com>
2018-10-12 15:11:14 -0700

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>This project uses {{ useCss }} for styling.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Just placeholder text',
+      useCss: 'Materialize'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
