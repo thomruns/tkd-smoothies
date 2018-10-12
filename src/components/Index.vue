ef588896f03f055dada37c4fa562918fb77b4f4a
Thom David <thomdavid74@gmail.com>
2018-10-12 16:54:08 -0700

    Add dummy data, style, print to screen

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="index">
-    <h1>{{ msg }}</h1>
-    <p>This project uses {{ useCss }} for styling.</p>
+  <div class="index container">
+    <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
+      <div class="card-content">
+        <h2 class="green-text">{{ smoothie.title }}</h2>
+        <ul class="ingredients">
+          <li v-for="(ing, index) in smoothie.ingredients" :key=index>
+            <span class="chip">{{ ing }}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,14 +18,34 @@ export default {
   name: 'Index',
   data () {
     return {
-      msg: 'Just placeholder text',
-      useCss: 'Materialize'
+      smoothies: [
+        { title: 'Green Goblin', slug: 'green-goblin', ingredients: ['spinach', 'spirulina', 'coconut milk'], id: '1'},
+        { title: 'Power Lunch', slug: 'power-lunch', ingredients: ['maca powder', 'kale', 'pineapple'], id: '2'}
+      ]
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
+.index {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 30px;
+  margin-top: 60px;
+}
 
+.index h2 {
+  font-size: 1.8em;
+  text-align: center;
+  margin-top: 0px;
+}
+
+.index .ingredients {
+  margin: 30px auto;
+}
+
+.index .ingredients li {
+  display: inline-block;
+}
 </style>
