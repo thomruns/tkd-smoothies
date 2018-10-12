89f75ef176962e5949b5346476a2b3d7877da872
Thom David <thomdavid74@gmail.com>
2018-10-12 16:22:56 -0700

    Navbar component, basic styling

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
+    <Navbar/>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Navbar
+  }
 }
 </script>
 
---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="navbar">
+    <nav class="nav-extended green darken-2">
+      <div class="nav-content">
+        <router-link to="">
+          <span class="nav-title">Green Smoothies</span>
+        </router-link>
+        <a href="" class="btn-floating btn-large halfway-fab purple darken-1">
+          <router-link to="">
+            <i class="material-icons">add</i>
+          </router-link>
+        </a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+  .navbar nav {
+    padding: 0 20px;
+  }
+</style>
+
