e687e12d5b633d0c03f8b112b61d0929eed61ff7
Thom David <thomdavid74@gmail.com>
2018-10-14 10:17:10 -0700

    Create form for adding smoothie

--- src/components/AddSmoothie.vue
+++ src/components/AddSmoothie.vue
@@ -1,5 +1,20 @@
 <template>
-  <h2>Add Smoothie Test Only</h2>
+  <div class="add-smoothie container">
+    <h2 class="center-align green-text text-darken-2">Add new smoothie recipe</h2>
+    <form>
+      <div class="field title">
+        <label for="title">Smoothie Title</label>
+        <input type="text" name="title" v-model="title">
+      </div>
+      <div class="field add-ingredient">
+        <label for="add-ingredient">Add an ingredient</label>
+        <input type="text" name="add-ingredient">
+      </div>
+      <div class="field center-align">
+        <button class="btn purple darken-1">Add Smoothie</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -7,14 +22,31 @@ export default {
   name: 'AddSmoothie',
   data() {
     return {
-
+      title: null
     }
   }
 }
 </script>
 
 <style>
+.add-smoothie {
+  margin-top: 60px;
+  padding: 20px;
+  max-width: 500px;
+}
 
+.add-smoothie h2 {
+  font-size: 2em;
+  margin: 20px auto;
+}
+
+.add-smoothie .field {
+  margin: 20px auto;
+}
+
+.add-smoothie button {
+  border-radius: 8px;
+}
 </style>
 
 
