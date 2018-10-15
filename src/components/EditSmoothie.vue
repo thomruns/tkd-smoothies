60b452ff37b4b427e45c3b930b6b5b485abb369f
Thom David <thomdavid74@gmail.com>
2018-10-15 11:37:12 -0700

    Wire edit view to database

--- src/components/EditSmoothie.vue
+++ src/components/EditSmoothie.vue
@@ -1,14 +1,30 @@
 <template>
-  <div class="edit-smoothie container">
-    <h2>Edit the {{ this.$route.params.smoothie_slug }} smoothie</h2>
+  <div v-if="smoothie" class="edit-smoothie container">
+    <h2>Edit {{ smoothie.title }}</h2>
+    <p>Ingredients: {{ smoothie.ingredients }}</p>
+    <p>Slug: {{ smoothie.slug }}</p>
+    <p>Document ID: {{ smoothie.id }}</p>
   </div>
 </template>
 
 <script>
+import db from '@/firebase/init' //import the Firebase database
+
 export default {
   name: 'EditSmoothie',
   data() {
-    return {}
+    return {
+      smoothie: null
+    }
+  },
+  created() {
+    let ref = db.collection('smoothies').where('slug', '==', this.$route.params.smoothie_slug)
+    ref.get().then(snapshot => {
+      snapshot.forEach(doc => {
+        this.smoothie = doc.data()
+        this.smoothie.id = doc.id
+      })
+    })
   }
 }
 </script>
