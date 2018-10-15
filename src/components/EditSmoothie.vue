fb81ccf869857e0c910a0fa19be4be67cece47fb
Thom David <thomdavid74@gmail.com>
2018-10-15 11:06:35 -0700

    Edit button and edit route added

---
+++ src/components/EditSmoothie.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="edit-smoothie container">
+    <h2>Edit the {{ this.$route.params.smoothie_slug }} smoothie</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EditSmoothie',
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style>
+
+</style>
+
