33604c56b1a521b141651422d1e2d23c32569603
Thom David <thomdavid74@gmail.com>
2018-10-15 12:47:29 -0700

    Basic Edit Form Created

--- src/components/EditSmoothie.vue
+++ src/components/EditSmoothie.vue
@@ -1,9 +1,25 @@
 <template>
   <div v-if="smoothie" class="edit-smoothie container">
     <h2>Edit {{ smoothie.title }}</h2>
-    <p>Ingredients: {{ smoothie.ingredients }}</p>
-    <p>Slug: {{ smoothie.slug }}</p>
-    <p>Document ID: {{ smoothie.id }}</p>
+    <form @submit.prevent="EditSmoothie">
+      <div class="field title">
+        <label for="title">Smoothie Name</label>
+        <input type="text" name="title" v-model="smoothie.title">
+      </div>
+      <div v-for="(ing, index) in smoothie.ingredients" :key="index" class="field">
+        <label for="ingredient">Ingredients:</label>
+        <input type="text" name="ingredient" v-model="smoothie.ingredients[index]">
+        <i class="material-icons delete" @click="deleteIng(ing)">delete</i>
+      </div>
+      <div class="field add-ingredient">
+        <label for="add-ingredient">Add an ingredient, (press Tab key to add another)</label>
+        <input type="text" name="add-ingredient" @keydown.tab.prevent="addIng" v-model="another">
+      </div>
+      <p v-if="feedback" class="red-text">{{ feedback }}</p>
+      <div class="field center-align">
+        <button class="btn purple darken-1">Update Smoothie</button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -14,7 +30,9 @@ export default {
   name: 'EditSmoothie',
   data() {
     return {
-      smoothie: null
+      smoothie: null,
+      another: null,
+      feedback: null
     }
   },
   created() {
@@ -25,11 +43,42 @@ export default {
         this.smoothie.id = doc.id
       })
     })
+  },
+  methods: {
+    editSmoothie() {
+      console.log(this.smoothie.title)
+    }
   }
 }
 </script>
 
 <style>
+.edit-smoothie {
+  margin-top: 60px;
+  padding: 20px;
+  max-width: 500px;
+}
 
+.edit-smoothie h2 {
+  font-size: 2em;
+  margin: 20px auto;
+}
+
+.edit-smoothie .field {
+  margin: 20px auto;
+  position: relative;
+}
+
+.edit-smoothie button {
+  border-radius: 8px;
+}
+.edit-smoothie .delete {
+  position: absolute;
+  right: 0;
+  bottom: 16px;
+  color: #aaa;
+  font-size: 1.4em;
+  cursor: pointer;
+}
 </style>
 
